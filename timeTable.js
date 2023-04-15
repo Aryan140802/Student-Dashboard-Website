6dedf28249841a73bfb64c2a4e57a43030f9da19
Aryan140802 <125740477+Aryan140802@users.noreply.github.com>
2023-04-15 12:54:36 +0530

    Add files via upload

---
+++ timeTable.js
@@ -0,0 +1,157 @@
+const Sunday =[
+    {   
+        time: 'Sunday',
+       subject: 'No classes',
+        teacher: 'No teacher',
+        roomNumber: 'Holiday',
+    }
+]
+const Monday =[
+   { time:'11:15-12:15 PM',
+    subject:'DSP',
+    teacher:'AHU',
+    roomNumber:'241'
+},
+    
+ { 
+     time:'1:00-2:00 PM',
+    subject:'OT/BSSP',
+    teacher:'DNP/SN',
+    roomNumber:'247' 
+},
+{
+    
+    time:'2:00-3:00 PM',
+    subject:'IPC',
+    teacher:'JKD',
+    roomNumber:'241' 
+},
+    
+    {
+    time:'3:00-5:00 PM',
+    subject:'IPC/DSP/PYT/MP',
+    teacher:'JKD/AHU/SVN/JBM',
+    roomNumber:'203/247/247/249'
+},
+    
+]
+const Tuesday =[
+    {   
+        time:'10:15-11:15 AM',
+        subject:'DSP',
+        teacher:'AHU',
+        roomNumber:'241' 
+    },
+        
+        
+     {   time:'11:15-12:15 PM',
+        subject:'PYT',
+        teacher:'SVM',
+        roomNumber:'241' 
+    },
+    {        
+        time:'1:00-2:00 PM',
+        subject:'IPC',
+        teacher:'JKD',
+        roomNumber:'241' 
+},
+   {
+        time:'2:00-3:00 PM',
+        subject:'IDC',
+        teacher:'AMG',
+        roomNumber:'241' 
+},       
+ {       time:'3:00-5:00 PM',
+        subject:'DSP/PYT/MP/IPC',
+        teacher:'AHU/SVN/JBM/PJP',
+        roomNumber:'247/247/248/203' 
+        
+    },
+]
+
+const Wednesday =[
+    {   
+        time: '10:00-5:00 AM',
+        subject: 'MINI PROJECT',
+        teacher: 'SN',
+        roomNumber:'245/247/249'
+    },
+    
+]
+
+const Thursday =[
+    { 
+     time:'10:15-11:15 AM',
+    subject:'ISP',
+    teacher:'JKD',
+    roomNumber:'242' 
+    },
+    
+    {
+    time:'11:15-12:15 PM',
+    subject:'PYT',
+    teacher:'SVM',
+    roomNumber:'242' 
+    },
+    {
+    time:'1:00-2:00 PM',
+    subject:'IDC',
+    teacher:'AMG',
+    roomNumber:'247' 
+    },
+    {
+    time:'2:00-3:00 PM',
+    subject:'OT/BSSP',
+    teacher:'DNP/SN',
+    roomNumber:'247' 
+    },
+    {
+    time:'3:00-5:00 PM',
+    subject:'MP/IPC/DSP/PYT',
+    teacher:'JBM/PJP/AHU/SVN',
+    roomNumber:'248/203/247/247' 
+    
+    },
+]
+
+const Friday =[
+    {   
+        time:'10:15-11:15 AM',
+        subject:'DSP',
+        teacher:'AHU',
+        roomNumber:'241' 
+    },
+    {
+        
+        time:'11:15-12:15 AM',
+        subject:'IDC',
+        teacher:'AMG',
+        roomNumber:'241' 
+    },
+    {   
+        time:'01:00-02:00 AM',
+        subject:'OT/BSSP',
+        teacher:'DNP/SN',
+        roomNumber:'241' 
+    },
+    {   
+        time:'2:00-4:00 PM',
+        subject:'PYT/MP/IPC/DSP',
+        teacher:'SVN/JBM/PJP/AHU',
+        roomNumber:'247/248/203/247' 
+        
+        
+        
+    },
+
+]
+
+const Saturday =[
+    {   
+        time: 'Saturday',
+       subject: 'No classes',
+        teacher: 'No teacher',
+        roomNumber: 'Holiday'
+    },
+  
+]
